dd19ba6f379a8b5e26b9ce8cf862b9de66078aa6
Vishal Sindham <vishalsindhamwork@gmail.com>
2026-07-14 14:17:03 +0530

    Learning how to use TavilySearch tool using a basic class on the package

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 from dotenv import load_dotenv
 from langchain.agents import create_agent
-from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 
 
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args :
-        query : The query to search for
-    Returns :
-        The search result
-    """
-    print(f"Searching for {query}")
-    return "Tokyo weather is sunny"
-
-
 llm = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 
